97e67bffc95ec7e505dc32c88c2302f17ff759b9
Marcin Niedbalec <marcin.niedbalec@proton.me>
2025-05-02 14:45:26 +0200

    add api prefix / enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,29 @@ import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { AllConfigType, fastifyAdapter } from './config';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     fastifyAdapter,
+    {
+      cors: true,
+    },
   );
   const configService = app.get(ConfigService<AllConfigType>);
 
-  const appPort = configService.getOrThrow('app.port', { infer: true });
-  await app.listen(appPort ?? 3000);
+  app.enableShutdownHooks();
+  app.setGlobalPrefix(
+    configService.getOrThrow('app.apiPrefix', { infer: true }),
+    {
+      exclude: ['/'],
+    },
+  );
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  await app.listen(configService.getOrThrow('app.port', { infer: true }));
 }
 bootstrap();
